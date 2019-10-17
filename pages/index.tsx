13fb0b8f677071626b607243961ca5322456afa0
Adetola Onasanya <adetola.onasanya@gmail.com>
2019-10-17 20:41:11 +0100

    Add styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import { NextPage } from 'next';
+import { App } from '../components/app.component';
 
 interface Props {
   userAgent?: string;
 }
 
-const Page: NextPage<Props> = ({ userAgent }) => <main>Your user agent: {userAgent}</main>;
+const Page: NextPage<Props> = ({ userAgent }) => (
+  <>
+    <App />
+    <main>Your user agent: {userAgent}</main>
+  </>
+);
 
 Page.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
