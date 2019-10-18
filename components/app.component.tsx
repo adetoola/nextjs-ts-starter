6ac7b0a3b08b8daa3de5bad99eb9fde47d6f54b8
Adetola Onasanya <adetola.onasanya@gmail.com>
2019-10-18 11:46:58 +0100

    styled-components and SSR

--- components/app.component.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: slategrey;
-  font-size: 18px;
-`;
-
-export const App: React.FC = () => <Container>Nextjs starter</Container>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
 import { NextPage } from 'next';
-import { App } from '../components/app.component';
 
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
 interface Props {
   userAgent?: string;
 }
 
-const Page: NextPage<Props> = ({ userAgent }) => (
-  <>
-    <App />
-    <main>Your user agent: {userAgent}</main>
-  </>
-);
+const Page: NextPage<Props> = ({ userAgent }) => <Title>Your user agent: {userAgent}</Title>;
 
 Page.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
